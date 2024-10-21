3c33f94d109a10932d44c8fc543ad6d8d7bd962e
hbd <hadrien.bigo-balland@myfit-solutions.com>
2024-10-21 15:03:38 +0200

    Added button to go on top of the page.

--- app.js
+++ app.js
@@ -116,3 +116,30 @@ function resizeIframe() {
 // Call on load and resize
 window.addEventListener('load', resizeIframe);
 window.addEventListener('resize', resizeIframe);
+
+// Get the button
+var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+scrollToTopBtn.onclick = function() {
+    scrollToTop();
+};
+
+function scrollToTop() {
+    // For smooth scrolling
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
